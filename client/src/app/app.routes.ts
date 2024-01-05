39c916e17ecf2572c101bac1d30db283ce1340b3
rachels-archive <rachelhui@protonmail.com>
2024-01-05 16:52:26 +0800

    👌 IMPROVE: Add cards to display resources

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,19 +4,19 @@ import { ForumComponent } from './pages/forum/forum.component';
 import { HomeComponent } from './pages/home/home.component';
 import { JobDetailsComponent } from './pages/job-details/job-details.component';
 import { JobListingComponent } from './pages/job-listing/job-listing.component';
-import { JobsComponent } from './pages/jobs/jobs.component';
 import { LoginComponent } from './pages/login/login.component';
 import { MyJobsComponent } from './pages/my-jobs/my-jobs.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ResourcesComponent } from './pages/resources/resources.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'jobs', component: JobsComponent },
+  { path: 'resources', component: ResourcesComponent },
   { path: 'job-details', component: JobDetailsComponent },
   { path: 'job-listing', component: JobListingComponent },
   { path: 'my-jobs', component: MyJobsComponent },
